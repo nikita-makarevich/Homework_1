b9799a606896f1b19ff3a89c3b40acff461ba49b
nikita-makarevich <nikitosmak0@gmail.com>
2024-01-30 19:08:45 +0300

    Area of a circle inscribed in a triangle

---
+++ app.py
@@ -0,0 +1,8 @@
+π = 3.14
+a = float(input(">>> Value 'a'="))
+b = float(input(">>> Value 'b'="))
+c = float(input(">>> Value 'c'="))
+A = float(input(">>> Value 'A'="))
+p = (a+b+c)/2
+S = π*((p-a)*match.tan(A/2))**2
+print(S)
